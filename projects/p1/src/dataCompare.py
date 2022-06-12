957d610587ef4efa0e93e4c9e47827764fbab5a9
joeylam <joey.cm.lam@gmail.com>
2022-06-12 17:18:43 +0800

    add postgresql db

--- projects/p1/src/dataCompare.py
+++ projects/p1/src/dataCompare.py
@@ -1,21 +1,29 @@
 import logging
 import datacompy
 import pandas as pd
+import psycopg2
 
+def getdata(sql):
+    conn = psycopg2.connect( database="mysys", user='dbadmin', password='dbadmin1234', host='127.0.0.1', port= '5432')
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    logging.info(data)
+    conn.close()
+    return data
 
 def main():
     try:
         logging.info("main")
-        df1 = pd.read_excel("../data/nav-s1.xlsx")
-        df2 = pd.read_excel("../data/nav-s2.xlsx")
-
+        df1 = pd.DataFrame( getdata("select * from mydb.t1; "), columns=["fnd_id", "nav_price"])
+        df2 = pd.DataFrame( getdata("select * from mydb.t2; "), columns=["fnd_id", "nav_price"])
         compare = datacompy.Compare(df1, df2, join_columns="fnd_id")
-        logging.info(compare)
+        logging.info(compare.report())
     except Exception as ex:
         logging.error(ex)
         raise Exception(ex)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s <%(levelname)s: %(message)s")
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s :%(levelname)s: %(message)s")
     main()
