8500818c47cbe99c61c4afbd47da8e05940aae7c
joeylam <joey.cm.lam@gmail.com>
2022-06-12 12:21:43 +0800

    compare 2 files with key

--- projects/p1/src/dataCompare.py
+++ projects/p1/src/dataCompare.py
@@ -1,8 +1,21 @@
 import logging
+import datacompy
+import pandas as pd
+
 
 def main():
-    logging.info("main")
+    try:
+        logging.info("main")
+        df1 = pd.read_excel("../data/nav-s1.xlsx")
+        df2 = pd.read_excel("../data/nav-s2.xlsx")
+
+        compare = datacompy.Compare(df1, df2, join_columns="fnd_id")
+        logging.info(compare)
+    except Exception as ex:
+        logging.error(ex)
+        raise Exception(ex)
+
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s <%(levelname)s: %(message)s")
     main()
