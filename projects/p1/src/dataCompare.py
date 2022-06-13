18a64630b8981d1f3a05ce8626067b215a307c65
joeylam <joey.cm.lam@gmail.com>
2022-06-13 21:16:02 +0800

    send email function

--- projects/p1/src/dataCompare.py
+++ projects/p1/src/dataCompare.py
@@ -3,7 +3,10 @@ import datacompy
 import pandas as pd
 from DataSource import DataSource
 from PGDBConn import PGDBConn
-
+from MSDBConn import MSDBConn
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
 class dataCompare:
 
@@ -16,13 +19,34 @@ class dataCompare:
             logging.error(ex)
             raise Exception (ex)
 
+    def sendReport(self, report):
+        # logging.info(report.report())
+        # The mail addresses and password
+        sender_address = 'xxxxgmail.com'
+        sender_pass = 'xxx'
+        receiver_address = 'xxx@gmail.com'
+        # Setup the MIME
+        message = MIMEMultipart()
+        message['From'] = sender_address
+        message['To'] = receiver_address
+        message['Subject'] = 'A test mail sent by Python. It has an attachment.'  # The subject line
+        # The body and the attachments for the mail
+        # message.attach(MIMEText(mail_content, 'plain'))
+        # Create SMTP session for sending the mail
+        session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+        session.starttls()  # enable security
+        session.login(sender_address, sender_pass)  # login with mail_id and password
+        text = report.report()
+        session.sendmail(sender_address, receiver_address, text)
+        session.quit()
+
     def run(self):
         try:
             logging.info("main")
             df1 = pd.read_excel("../data/nav-s1.xlsx", sheet_name='Sheet1',converters={'fnd_id':str,'fnd_ver':str})
             df2 = pd.DataFrame( self.getDatafromDB(self._datasource, "select * from mydb.t2;"), columns=["fnd_id", "fnd_ver", "nav_price"])
             compare = datacompy.Compare(df1, df2, join_columns=["fnd_id", "fnd_ver"])
-            logging.info(compare.report())
+            self.sendReport(compare)
         except Exception as ex:
             logging.error(ex)
             raise Exception(ex)
@@ -33,7 +57,9 @@ class dataCompare:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s :%(levelname)s: %(name)s: %(message)s")
     try:
-        app = dataCompare(PGDBConn("mysys", "dbadmin", "dbadmin1234", "127.0.0.1", "5432"))
+        db = PGDBConn("mysys", "dbadmin", "dbadmin1234", "127.0.0.1", "5432")
+        # db = MSDBConn("mysys", "sa", "DBadmin1234", "127.0.0.1", "1433")
+        app = dataCompare(db)
         app.run()
         SystemExit(0)
     except Exception as ex:
