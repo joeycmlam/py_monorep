6594d9c282fa168321a6ff6348cfc1a9b793a140
joeylam <joey.cm.lam@gmail.com>
2022-06-13 21:44:36 +0800

    reflectoring on email

--- projects/p1/src/dataCompare.py
+++ projects/p1/src/dataCompare.py
@@ -20,20 +20,6 @@ class dataCompare:
     def sendReport(self, report):
         # logging.info(report.report())
         # The mail addresses and password
-        sender_address = 'joey.cm.lam@gmail.com'
-        sender_pass = 'xuifotddmmkzclnk'
-        receiver_address = 'joey.cm.lam@gmail.com'
-        # Setup the MIME
-        message = MIMEMultipart()
-        message['From'] = sender_address
-        message['To'] = receiver_address
-        message['Subject'] = 'A test mail sent by Python. It has an attachment.'  # The subject line
-        # The body and the attachments for the mail
-        # message.attach(MIMEText(mail_content, 'plain'))
-        # Create SMTP session for sending the mail
-        session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
-        session.starttls()  # enable security
-        session.login(sender_address, sender_pass)  # login with mail_id and password
         text = report.report()
         session.sendmail(sender_address, receiver_address, text)
         session.quit()
